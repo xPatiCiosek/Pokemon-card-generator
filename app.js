007c654de2308d641a121c68270a5f345a93b549
xPatiCiosek <patrycja_ciosek@hotmail.com>
2024-03-23 22:13:55 +0000

    added func to change card color dependant on type

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+const gradients = {
+  'electric' : "linear-gradient(43deg, #ba8a08 0%, #d1a30b 46%, #dcc266 100%)",
+  'bug' : "linear-gradient(43deg, #527039 0%, #83a36f 46%, #aac784 100%)",
+  'dark' : "linear-gradient(43deg, #6f7e8d 0%, #4c6683 46%, #3a526b 100%",
+  'dragon' : "linear-gradient(43deg, #626e9c 0%, #4c6283 46%, #3c5087 100%)",
+  'fairy' : "linear-gradient(43deg, #c089bc 0%, #ab74a7 46%, #925d8f 100%)",
+  'fighting' : "linear-gradient(43deg, #9c5454 0%, #ba2f2f 46%, #912b2b 100%)",
+  'fire' : "linear-gradient(43deg, #c1642f 0%, #d9833d 46%, #d6c251 100%)",
+  'flying' : "linear-gradient(43deg, #2f87c1 0%, #478ab6 46%, #95d0f8 100%)",
+  'ghost' : "linear-gradient(43deg, #2f69c1 0%, #37698b 46%, #41769a 100%)",
+  'grass' : "linear-gradient(43deg, #487624 0%, #378b44 46%, #4a9a41 100%)",
+  'ground' : "linear-gradient(43deg, #655242 0%, #936045 46%, #6d4d3f 100%)",
+  'ice' : "linear-gradient(43deg, #49716c 0%, #458e93 46%, #68a1a2 100%)",
+  'normal' : "linear-gradient(43deg, #595b5d 0%, #5b6976 46%, #9b9d9f 100%)",
+  'poison' : "linear-gradient(43deg, #6c4971 0%, #794593 46%, #7e68a2 100%)",
+  'psychic' : "linear-gradient(43deg, #923859 0%, #ad4e71 46%, #d495ac 100%)",
+  'rock' : "linear-gradient(43deg, #635d41 0%, #756d4c 46%, #7a7663 100%)",
+  'steel' : "linear-gradient(43deg, #788ea2 0%, #456e93 46%, #425465 100%)",
+  'water' : "linear-gradient(43deg, #325d83 0%, #1c517f 46%, #537ca0 100%)"
+}
 const movesContainer = document.getElementById('moves-container');
 const baseURL = `https://pokeapi.co/api/v2/`;
 const POKEMON = 'pokemon/';
@@ -41,6 +61,10 @@ async function fetchFromTwoAPIs(pokeName) {
   } catch (error) {
       console.error('Error:', error);
   }
+}
+
+function changeCardStyle(type) {
+  document.getElementById('card').style.backgroundImage = gradients[type];
 }
 
 function renderMovesCard(data){
